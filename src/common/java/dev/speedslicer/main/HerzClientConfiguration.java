0c9027622ce0b6a6c3a3c6e1bcd470dcdcea6f4a
SpeedSlicer <mathewshaun25@gmail.com>
2026-08-02 15:44:01 -0400

    Really tiny change for preference in logging

--- src/common/java/dev/speedslicer/main/HerzClientConfiguration.java
+++ src/common/java/dev/speedslicer/main/HerzClientConfiguration.java
@@ -1,6 +1,6 @@
 package dev.speedslicer.main;
 
 public class HerzClientConfiguration {
-    public static final String clientBrandName = "Herz Example Client";
+    public static final String clientBrandName = "Herz Platform API";
     public static final String versionNumber = "0.0.0";
 }
