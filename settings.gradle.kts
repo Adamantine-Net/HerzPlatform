c1667c93921ae3cae8449dee4dc116e85b25073e
notanorange <NotAnOrangeEagler@proton.me>
2026-08-03 13:38:10 -0400

    add 1.5.2 submodule

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ rootProject.name = "HerzPlatform"
 
 includeBuild("modules/module-eag-1_8")
 includeBuild("modules/module-eag-1_14")
+includeBuild("modules/module-eag-1_5_2")
 
 include("mixin:mixin-loader")
 include("mixin:mixins-1_8")
